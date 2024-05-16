bce6cd519e8bf0ef40511cf924c459324e6f6088
Onur Aktas <onuraktas0640@gmail.com>
2024-05-16 15:13:44 +0300

    NewsLetter-ErrorPage

--- CoreDemo/BusinessLayer/Concrete/CommentManager.cs
+++ CoreDemo/BusinessLayer/Concrete/CommentManager.cs
@@ -9,22 +9,22 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-	public class CommentManager : ICommentService
-	{
-		ICommentDal _commentDal;
-		public CommentManager(ICommentDal commentDal)
-		{
-			_commentDal = commentDal;
+    public class CommentManager : ICommentService
+    {
+        ICommentDal _commentDal;
+        public CommentManager(ICommentDal commentDal)
+        {
+            _commentDal = commentDal;
 
-		}
-		public void Add(Comment comment)
-		{
-			throw new NotImplementedException();
-		}
+        }
+        public void Add(Comment comment)
+        {
+            _commentDal.Add(comment);
+        }
 
-		public List<Comment> GetAll(int id)
-		{
-			return _commentDal.GetAll(x => x.BlogId == id);
-		}
-	}
+        public List<Comment> GetAll(int id)
+        {
+            return _commentDal.GetAll(x => x.BlogId == id);
+        }
+    }
 }
--- CoreDemo/CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/CoreDemo/Controllers/CommentController.cs
@@ -1,25 +1,35 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
-	public class CommentController : Controller
-	{
-		CommentManager _commentManager = new CommentManager(new EfCommentRepository());
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public class CommentController : Controller
+    {
+        CommentManager _commentManager = new CommentManager(new EfCommentRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public PartialViewResult PartialAddComment()
-		{
-			return PartialView();
-		}
-		public PartialViewResult PartialCommentListByBlog(int id)
-		{
-			var data = _commentManager.GetAll(id);
-			return PartialView(data);
-		}
-	}
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult PartialAddComment(Comment p)
+        {
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.Status = true;
+            p.BlogId = 2;
+            _commentManager.Add(p);
+            return PartialView();
+        }
+        public PartialViewResult PartialCommentListByBlog(int id)
+        {
+            var data = _commentManager.GetAll(id);
+            return PartialView(data);
+        }
+    }
 }
