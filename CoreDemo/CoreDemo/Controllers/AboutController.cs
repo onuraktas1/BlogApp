7b9e51a396d3994dc4ec76aed9c55106edc0a9a8
Onur Aktas <onuraktas0640@gmail.com>
2024-05-31 15:16:26 +0300

    About

---
+++ CoreDemo/CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly AboutManager _aboutManager = new(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            List<About> data = _aboutManager.GetList();
+          
+            return View(data);
+        }
+
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ CoreDemo/CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class ContactController : Controller
+	{
+		private readonly ContactManager _contactManager = new(new EfContactRepository());
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Contact contact)
+		{
+			contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+			contact.Status = true;
+			_contactManager.Add(contact);
+			return RedirectToAction("Index", "Blog");
+		}
+	}
+}
