ab89d705bd2865bd9de89130feb477b70f8da1e6
Onur Aktas <onuraktas0640@gmail.com>
2024-08-08 14:09:04 +0300

    Dashboard

---
+++ CoreDemo/CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager _categoryManager = new(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var data = _categoryManager.GetAll();
+            return View(data);
+        }
+    }
+}
