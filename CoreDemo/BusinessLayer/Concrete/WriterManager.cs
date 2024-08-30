93986634010a11fce79eb8a38a8b705bc7d94510
Onur Aktas <onuraktas0640@gmail.com>
2024-08-30 20:01:37 +0300

    WriterEdit

--- CoreDemo/BusinessLayer/Concrete/WriterManager.cs
+++ CoreDemo/BusinessLayer/Concrete/WriterManager.cs
@@ -35,7 +35,7 @@ namespace BusinessLayer.Concrete
 
         public Writer GetById(int id)
         {
-            throw new NotImplementedException();
+            return _writerDal.GetById(id);
         }
 
         public List<Writer> GetWriterById(int id)
@@ -45,7 +45,7 @@ namespace BusinessLayer.Concrete
 
         public void Update(Writer t)
         {
-            throw new NotImplementedException();
+            _writerDal.Update(t);
         }
     }
 }
--- CoreDemo/CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/CoreDemo/Controllers/WriterController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class WriterController : Controller
     {
+        private readonly WriterManager _writerManager = new(new EfWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -30,6 +36,34 @@ namespace CoreDemo.Controllers
         public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
+        }
+        [AllowAnonymous]
+        public IActionResult WriterEditProfile()
+        {
+            var writerData = _writerManager.GetById(1);
+            return View(writerData);
+        }
+
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer writer)
+        {
+            WriterValidator writerValidator = new();
+            writer.Image = "Bos";
+            ValidationResult result = writerValidator.Validate(writer);
+
+            if (result.IsValid)
+            {
+                _writerManager.Update(writer);
+                return RedirectToAction("Index", "DashBoard");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
