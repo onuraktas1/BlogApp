efa2ea5026de1491140cb396ab75a66662fa5615
Onur Aktas <onuraktas0640@gmail.com>
2024-09-03 11:23:23 +0300

    Notification

---
+++ CoreDemo/CoreDemo/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Notification
+{
+    public class NotificationList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
