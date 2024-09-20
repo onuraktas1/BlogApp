dd07251e01e78f9427311e20d499c8527756d0e7
Onur Aktas <onuraktas0640@gmail.com>
2024-09-21 01:51:01 +0300

    Notification

--- CoreDemo/CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/CoreDemo/Controllers/NotificationController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class NotificationController : Controller
     {
+        private readonly NotificationManager _notificationManager = new(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var data = _notificationManager.GetAll();
+            return View(data);
         }
     }
 }
