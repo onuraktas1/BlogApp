0936d052adcfe7db51187ebaa865bbeecb55e534
Onur Aktas <onuraktas0640@gmail.com>
2025-01-02 01:31:56 +0300

    Page

--- CoreDemo/CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        [Area("Admin")]
-        public IActionResult Index()
+        private readonly CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page = 1)
         {
-            return View();
+            var categories = _categoryManager.GetAll().ToPagedList(page, 3);
+            return View(categories);
         }
     }
 }
