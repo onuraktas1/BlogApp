561e5d3893013c693c3554ada01b31d3d2497acf
Onur Aktas <onuraktas0640@gmail.com>
2025-01-06 02:34:44 +0300

    Statistic

---
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        private readonly BlogManager _blogManager = new(new EfBlogRepository());
+        private readonly Context _context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _blogManager.GetAll().Count;
+            ViewBag.v2 = _context.Contacts.Count();
+            ViewBag.v3 = _context.Comments.Count();
+            return View();
+        }
+    }
+}
