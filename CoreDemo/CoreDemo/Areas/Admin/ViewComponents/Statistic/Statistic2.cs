6da2112ac62c50266fe8e84e30517610febec3d2
Onur Aktas <onuraktas0640@gmail.com>
2025-01-06 17:20:59 +0300

    statistic

---
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        private readonly Context _context = new();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _context.Blogs.OrderByDescending(x => x.Id).Select(x => x.Title).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
