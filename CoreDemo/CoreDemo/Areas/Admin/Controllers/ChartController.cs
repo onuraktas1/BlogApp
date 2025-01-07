3586ceae5314c346877e6d62d752d8425e0134c8
Onur Aktas <onuraktas0640@gmail.com>
2025-01-08 01:34:32 +0300

    chart

---
+++ CoreDemo/CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,42 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> categoryClasses = new List<CategoryClass>();
+
+            categoryClasses.Add(new CategoryClass
+            {
+                CategoryName = "Teknoloji",
+                CategorytCount = 10
+            });
+            categoryClasses.Add(new CategoryClass
+            {
+                CategoryName = "Yazılım",
+                CategorytCount = 12
+            });
+            categoryClasses.Add(new CategoryClass
+            {
+                CategoryName = "Donanım",
+                CategorytCount = 5
+            });
+            categoryClasses.Add(new CategoryClass
+            {
+                CategoryName = "Spor",
+                CategorytCount = 4
+            });
+
+            return Json(new { jsonList = categoryClasses });
+        }
+    }
+}
