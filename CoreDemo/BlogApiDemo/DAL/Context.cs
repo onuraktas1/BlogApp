34f8b4e972ba4fbc0f737219b1f9678f1f9a3cda
Onur Aktas <onuraktas0640@gmail.com>
2025-03-31 15:25:01 +0300

    .net version update

--- CoreDemo/BlogApiDemo/DAL/Context.cs
+++ CoreDemo/BlogApiDemo/DAL/Context.cs
@@ -6,7 +6,9 @@ namespace BlogApiDemo.DAL
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=.;Database=BlogApiDb;Trusted_Connection=True;TrustServerCertificate=true");
+            // optionsBuilder.UseSqlServer("Server=.;Database=BlogApiDb;Trusted_Connection=True;TrustServerCertificate=true");
+            
+            optionsBuilder.UseSqlServer("Server=localhost;Database=BlogApiDb;User Id=SA;Password=Onuraktas1234;TrustServerCertificate=True;");
         }
 
         public DbSet<Employee> Employees { get; set; }
