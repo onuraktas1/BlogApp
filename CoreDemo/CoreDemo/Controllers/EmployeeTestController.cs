b4769af2782941d89a838205f97b02a3b39da118
Onur Aktas <onuraktas0640@gmail.com>
2025-04-08 03:31:39 +0300

    ApiTest

--- CoreDemo/CoreDemo/Controllers/EmployeeTestController.cs
+++ CoreDemo/CoreDemo/Controllers/EmployeeTestController.cs
@@ -33,6 +33,40 @@ public class EmployeeTestController : Controller
         }
         return View(p);
     }
+
+    public async Task<IActionResult> DeleteEmployee(int id)
+    {
+        var httpClient = new HttpClient();
+        var responseMessage = await httpClient.DeleteAsync($"https://localhost:7081/api/Default/{id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public async Task<IActionResult> EditEmployee(int id)
+    {
+        var httpClient = new HttpClient();
+        var responseMessage = await httpClient.GetAsync($"https://localhost:7081/api/Default/{id}");
+        var jsonString = await responseMessage.Content.ReadAsStringAsync();
+        var values =JsonConvert.DeserializeObject<Employee>(jsonString);
+        return View(values);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditEmployee(Employee p)
+    {
+        var httpClient = new HttpClient();
+        var jsonEmployee = JsonConvert.SerializeObject(p);
+        var content = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");
+        var responseMessage = await httpClient.PutAsync("https://localhost:7081/api/Default", content);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");   
+        }
+        return View(p);
+    }
 }
 
 public class Employee
