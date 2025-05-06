e6984c466a3453b561d9c54447ab10d00ea41a4f
Onur Aktas <onuraktas0640@gmail.com>
2025-05-07 00:45:35 +0300

    Update WriterMessageNotification.cs

--- CoreDemo/CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,13 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterMessageNotification : ViewComponent
     {
         private readonly Message2Manager _message2Manager = new(new EfMessage2Repository());
+        private readonly Context _context = new();
         public IViewComponentResult Invoke()
         {
-            int id = 1;
-            var data = _message2Manager.GetInboxListByWriter(id);
+            var userName = User.Identity.Name;
+            var userMail = _context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerId = _context.Writers.Where(x => x.Mail == userMail).Select(x => x.Id).FirstOrDefault();
+            var data = _message2Manager.GetInboxListByWriter(writerId);
             return View(data);
         }
     }
